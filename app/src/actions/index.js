a6966ef1272d8260ec5d3fd619f96b49bb873052
trean <trean.mi@gmail.com>
2018-01-24 19:58:38 +0300

    dropzone for details component

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,5 +1,4 @@
 export const select = (client) => {
-  //alert('Client is: ' + client.id);
   return {
     type: 'CLIENT_SELECTED',
     // data
--- app/src/containers/details.js
+++ app/src/containers/details.js
@@ -1,34 +1,78 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Dropzone from 'dropzone';
 
 class Details extends Component {
+
+  componentDidMount() {
+    function fileParamName() {
+      return 'files'
+    }
+    Dropzone.options.detDropzone = {
+      paramName       : fileParamName,
+      url             : 'http://localhost:8090/upload',
+      autoProcessQueue: false,
+      uploadMultiple  : true,
+      parallelUploads : 5,
+      maxFiles        : 5,
+      maxFilesize     : 60,
+      acceptedFiles   : '.doc,.docx,.pdf',
+      addRemoveLinks  : true,
+      init            : function () {
+        let dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
+
+        // for Dropzone to process the queue (instead of default form behavior):
+        document.getElementById("submit-all").addEventListener("click", function (e) {
+          // Make sure that the form isn't actually being sent.
+          e.preventDefault();
+          e.stopPropagation();
+          dzClosure.processQueue();
+        });
+
+        //send all the form data along with the files:
+        this.on("sendingmultiple", function (data, xhr, formData) {
+          formData.append("userName", document.getElementById("userName").value);
+        });
+      }
+    }
+
+  }
+
+
   render() {
     if (!this.props.client) {
       return (<p>Choose client.</p>);
     }
 
-
-    //function NumberList(props) {
+    // prepare files to view
     const files          = this.props.client.files;
-    const filesListItems = files.map((file) => {
+    const filesListItems = files.map((file, idx) => {
       let fileName = file.path.match(/[^\\]*\.(\w+)$/)[0] + '.' + file.path.match(/[^\\]*\.(\w+)$/)[1];
-      return <li><span>{fileName}</span></li>
+      return <li key={idx}><span>{fileName}</span><input type="checkbox"/></li>
     });
-    //}
 
     return (
       <div>
         <h3>{this.props.client.name}</h3>
-        <ul>
-          {filesListItems}
-        </ul>
-        <div>
-          I'm dropzone!
-        </div>
+        <form action="/delete-file" method="POST">
+          <ul>
+            {filesListItems}
+          </ul>
+          <button type="submit" onClick={(e) => e.preventDefault()}>Delete</button>
+        </form>
+
+        <form action="/upload" enctype="multipart/form-data" method="POST">
+          <input id="userName" type="text" name="userName" placeholder="Client Name"
+                 defaultValue={this.props.client.name} hidden/>
+          <div className="dropzone" id="detDropzone"></div>
+          <button type="submit" id="submit-all"> upload</button>
+
+        </form>
       </div>
     )
   }
 }
+
 
 // from state to property
 function mapStateToProps(state) {
