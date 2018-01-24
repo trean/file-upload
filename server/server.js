46d80f23150a004a6a3ffae700d666d253842bf1
trean <trean.mi@gmail.com>
2018-01-24 23:48:13 +0300

    added api for models to server, small server refactoring

--- server/server.js
+++ server/server.js
@@ -13,10 +13,17 @@ const express    = require('express'),
       Client     = require('./client-schema'),
 
       multer     = require('multer'),
+
+      rootDir    = path.join(__dirname, '..'),
+
+      staticDir  = path.join(rootDir, 'static'),
+
+      uploadsDir = path.join(staticDir, 'uploads' ),
+
       multerConf = {
         storage   : multer.diskStorage({
           destination: function (req, res, next) {
-            next(null, path.join(__dirname, '..', 'uploads'));
+            next(null, uploadsDir);
           },
           filename   : function (req, file, next) {
             //const ext = file.mimetype.split('/')[1];
@@ -63,6 +70,8 @@ app.engine('html', require('ejs').__express);
 app.set('views', __dirname + '/../app/build');
 app.set('view engine', 'ejs');
 
+app.use('/static', express.static(path.join(rootDir, 'static')))
+
 app.use('/', express.static(__dirname + '/../app/build'));
 app.get('/', function (req, res) {
   res.render('index.html');
@@ -81,9 +90,94 @@ app.post('/upload', function (req, res) {
       return null;
     }
     let client = new Client({clientName: req.body.userName});
-    processUploadedData(client, req);
-    res.send('this is upload');
+    processUploadedData(client, req, function(err, model){
+      if(err){
+        console.log(err);
+        res.status(400).send('error');
+      }else{
+        res.send('ok')
+      }
+    });
   });
+});
+
+
+/**
+ * This API will return list of all clients with ids and name
+ */
+app.get('/clients', function(req, res) {
+  Client.find({}, '_id clientName', function(err, docs){
+    res.send(docs);
+  })
+});
+
+
+/**
+ * This API will return full client object by id or 404 error if not found
+ */
+app.get('/client/:clientId', function(req, res){
+  Client.findOne({_id: req.params.clientId}, function(err, doc){
+    if(!err && doc){
+      res.send(doc)
+    }else{
+      res.status(404).send(err);
+    }
+  })
+});
+
+app.post('/client/:clientId/upload', function(req, res){
+  const clientId = req.params.clientId;
+  upload(req, res, function (err) {
+    if (err) {
+      console.error(err);
+      // An error occurred when uploading
+      return null;
+    }
+
+    Client.findOne({_id: clientId}, function(err, doc){
+      processUploadedData(client, req, function(err, model){
+        if(err){
+          console.log(err);
+          res.status(400).send('error');
+        }else{
+          res.send('ok')
+        }
+      });
+    })
+  });
+
+});
+
+/**
+ * This API will delete particular file for particular user.
+ */
+app.delete('/client/:clientId/file/:fileId', function(req, res){
+  const clientId = req.params.clientId;
+  const fileId = req.params.fileId;
+  Client.findOne({ _id: clientId }, function(err, doc){
+    if(!err && doc){
+
+      const files = doc.get("files");
+
+      let fileToRemove = null;
+
+      const filesToKeep = [];
+
+      files.forEach(file => { if (file._id == fileId){ fileToRemove = file }else{ filesToKeep.push(file) }});
+
+      if(fileToRemove){
+        doc.set('files', filesToKeep);
+        doc.save().then((doc) => {
+          let filePath = path.join(staticDir, fileToRemove.path);
+          fs.unlink(filePath, (err) => res.send('ok'));
+        }).catch((err) => res.status(400).send(err))
+      }else{
+        res.status(404).send("File not found")
+      }
+    }else{
+      res.status(404).send(err);
+    }
+  })
 });
 
 
@@ -94,14 +188,14 @@ app.post('/upload', function (req, res) {
 function createClientDir(dir) {
   return new Promise((resolve, reject) => {
     if (!fs.existsSync(dir)) {
-      let pathToUserDir = path.join(__dirname, '..', 'uploads', dir);
+      let pathToUserDir = path.join(uploadsDir, dir);
       fs.mkdirSync(pathToUserDir);
       resolve(pathToUserDir)
     } else {
       reject('path exists');
     }
 
-  }).catch(err => console.err(err));
+  });
 }
 
 
@@ -115,11 +209,11 @@ function moveFileToUserDir(oldPath, newPath) {
       }
       resolve(newPath);
     });
-  }).catch(err => console.error(err));
+  });
 }
 
 
-function processUploadedData(client, req) {
+function processUploadedData(client, req, callback) {
   let userId = null;
   client.save().then((model) => {
     userId = model._id;
@@ -129,28 +223,37 @@ function processUploadedData(client, req) {
       req.files.forEach(file => {
         let processedFileName = /\s/.test(file.originalname) ? file.originalname.split(' ').join('_') : file.originalname,
             // TODO: fix paths
-            oldPathToFile     = path.join(__dirname, '..', 'uploads', file.originalname),
+            oldPathToFile     = path.join(uploadsDir, file.originalname),
             newPathToFile     = path.join(pathToUserDir, processedFileName);
         moveFileToUserDir(oldPathToFile, newPathToFile)
           .then(newFilePath => {
-            updateClientFilesAtDB(Client, newFilePath, userId);
-          }).catch(error => console.log(error));
+            updateClientFilesAtDB(model, newFilePath, callback);
+          }).catch(callback);
       })
     })
-    .catch(error => console.log(error));
+    .catch(callback);
 }
 
 
 // TODO: update array
-function updateClientFilesAtDB(client, pathToFil, clientid) {
-  client.findByIdAndUpdate(
-    clientid,
-    {$set: {'files': {path: pathToFil}}},
-    {
-      upsert: true
-    },
-    function (err, model) {
-      console.log(err, model);
-    }
-  );
+function updateClientFilesAtDB(client, pathToFile, callback) {
+  let files = client.get('files');
+  files = files.concat([{path: pathToFile}]);
+  client.set('files', files);
+  client.save().then((model) => callback(null, model)).catch(callback)
 }
+
+
+app.get('/client/:clientId/test', function(req, res){
+  Client.findOne({_id: req.params.clientId}, function(err, doc){
+    if(!err && doc){
+      updateClientFilesAtDB(doc, "TEST_PATH2", (err, model) => {
+        if(err) res.status(400)
+        console.log(err, model);
+        res.send(model)
+      })
+    }else{
+      res.status(404).send(err);
+    }
+  })
+})
